179a5b3f28b98f397a200b0abe122e524321169c
Kiri <kirilynne@gmail.com>
2021-10-25 20:35:23 -0400

    update to include switch cases instead of if/else

--- server.js
+++ server.js
@@ -29,62 +29,145 @@ const userOption = [
     }
 ];
 
-const newDepartment = [
-    {
-        type: 'input',
-        message: 'Please enter the name of the department you would like to add.',
-        name: 'department',
-        validate: (value) => { if (value) { return true } else { return 'Must enter a department name to continue.' } }
-    }
-];
+function init() {
+    inquirer.prompt(userOption)
+        .then((responses) => {
+            switch (responses.option) {
+                case "view all departments": viewDepartments(); break;
+                case "view all roles": viewRoles(); break;
+                case "view all employees": viewEmployees(); break;
+                case "add a department": addDepartment(); break;
+                case "add a role": addRole(); break;
+                case "add an employee": addEmployee(); break;
+                case "update an employee role": updateEmployee(); break;
+                case "exit": dbConnection.end(); break;
+            }
+        })
+}
 
-const newRole = [
-    {
-        type: 'input',
-        message: 'Please enter the name of the role you would like to add.',
-        name: 'role',
-        validate: (value) => { if (value) { return true } else { return 'Must enter a role name to continue.' } }
-    },
-    {
-        type: 'input',
-        message: 'Please enter the salary for the role you would like to add.',
-        name: 'roleSalary',
-        validate: (value) => { if (value) { return true } else { return 'Must enter a salary to continue.' } }
-    },
-    {
-        type: 'input',
-        message: 'Please enter the department ID for the role you would like to add.',
-        name: 'roleDepartment',
-        validate: (value) => { if (value) { return true } else { return 'Must enter a department to continue.' } }
-    }
-];
+function viewDepartments() {
+    dbConnection.query(`SELECT * FROM department`, (err, res) => {
+        if (err)
+            throw err;
+        console.table(res);
+        init();
+    })
+}
 
-const newEmployee = [
-    {
-        type: 'input',
-        message: 'Please enter the first name of the employee you would like to add.',
-        name: 'firstName',
-        validate: (value) => { if (value) { return true } else { return 'Must enter a first name to continue.' } }
-    },
-    {
-        type: 'input',
-        message: 'Please enter the last name of the employee you would like to add.',
-        name: 'lastName',
-        validate: (value) => { if (value) { return true } else { return 'Must enter a last name to continue.' } }
-    },
-    {
-        type: 'input',
-        message: 'Please enter the role ID of the employee you would like to add.',
-        name: 'employeeRole',
-        validate: (value) => { if (value) { return true } else { return 'Must enter the role to continue.' } }
-    },
-    {
-        type: 'input',
-        message: 'Please enter the manager ID of this new employee.',
-        name: 'manager',
-        validate: (value) => { if (value) { return true } else { return 'Must enter a manager to continue.' } }
-    },
-];
+function viewRoles() {
+    dbConnection.query(`SELECT * FROM role`, (err, res) => {
+        if (err)
+            throw err;
+        console.table(res);
+        init();
+    })
+}
+
+function viewEmployees() {
+    dbConnection.query(`SELECT * FROM employee`, (err, res) => {
+        if (err)
+            throw err;
+        console.table(res);
+        init();
+    })
+}
+
+function addDepartment() {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message: 'Please enter the name of the department you would like to add.',
+                name: 'department',
+                validate: (value) => { if (value) { return true } else { return 'Must enter a department name to continue.' } }
+            }
+        ]
+    )
+        .then((responses) => {
+            dbConnection.query("INSERT INTO department SET ?",
+                { name: responses.department }, (err, res) => {
+                    if (err)
+                        throw err;
+                    console.log("You successfully added a new department.")
+                    init();
+                })
+        })
+}
+
+function addRole() {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message: 'Please enter the name of the role you would like to add.',
+                name: 'role',
+                validate: (value) => { if (value) { return true } else { return 'Must enter a role name to continue.' } }
+            },
+            {
+                type: 'input',
+                message: 'Please enter the salary for the role you would like to add.',
+                name: 'roleSalary',
+                validate: (value) => { if (value) { return true } else { return 'Must enter a salary to continue.' } }
+            },
+            {
+                type: 'input',
+                message: 'Please enter the department ID for the role you would like to add.',
+                name: 'roleDepartment',
+                validate: (value) => { if (value) { return true } else { return 'Must enter a department to continue.' } }
+            }
+        ]
+    )
+        .then((responses) => {
+            dbConnection.query("INSERT INTO role SET ?",
+                { title: responses.role, salary: pareseInt(responses.roleSalary), department_id: parseInt(resposnes.roleDepartment) }, (err, res) => {
+                    if (err)
+                        throw err;
+                    console.log("You successfully added a new role.")
+                    init();
+                });
+        })
+}
+
+function addEmployee() {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                message: 'Please enter the first name of the employee you would like to add.',
+                name: 'firstName',
+                validate: (value) => { if (value) { return true } else { return 'Must enter a first name to continue.' } }
+            },
+            {
+                type: 'input',
+                message: 'Please enter the last name of the employee you would like to add.',
+                name: 'lastName',
+                validate: (value) => { if (value) { return true } else { return 'Must enter a last name to continue.' } }
+            },
+            {
+                type: 'input',
+                message: 'Please enter the role ID of the employee you would like to add.',
+                name: 'employeeRole',
+                validate: (value) => { if (value) { return true } else { return 'Must enter the role to continue.' } }
+            },
+            {
+                type: 'input',
+                message: 'Please enter the manager ID of this new employee.',
+                name: 'manager',
+                validate: (value) => { if (value) { return true } else { return 'Must enter a manager to continue.' } }
+            },
+        ]
+    )
+        .then((responses) => {
+            dbConnection.query("INSERT INTO employee SET ?",
+                { first_name: responses.firstName, last_name: responses.lastName, employee_role_id: parseInt(responses.employeeRole), manager_id: parseInt(responses.manager) }, (err, res) => {
+                    if (err)
+                        throw err;
+                    console.log("You successfully added a new employee.")
+                    init();
+                });
+        })
+}
+
 
 function updateEmployee() {
     dbConnection.query("SELECT employee_id, first_name, las_name FROM employee", (err, res) => {
@@ -118,71 +201,3 @@ function updateEmployee() {
             })
     })
 }
-
-function init() {
-    inquirer.prompt(userOption)
-        .then((responses) => {
-            if (responses.option === "view all departments") {
-                dbConnection.query(`SELECT * FROM department`, (err, res) => {
-                    if (err)
-                        throw err;
-                    console.table(res);
-                    init();
-                });
-            } else if (responses.option = "view all roles") {
-                dbConnection.query(`SELECT * FROM role`, (err, res) => {
-                    if (err)
-                        throw err;
-                    console.table(res);
-                    init();
-                });
-            } else if (responses.option = "view all employees") {
-                dbConnection.query(`SELECT * FROM employee`, (err, res) => {
-                    if (err)
-                        throw err;
-                    console.table(res);
-                    init();
-                });
-            } else if (responses.option = "add a department") {
-                inquirer.prompt(newDepartment)
-                    .then((responses) => {
-                        dbConnection.query("INSERT INTO department SET ?",
-                            { name: responses.department }, (err, res) => {
-                                if (err)
-                                    throw err;
-                                console.log("You successfully added a new department.")
-                                init();
-                            });
-                    });
-            } else if (responses.option = "add a role") {
-                inquirer.prompt(newRole)
-                    .then((responses) => {
-                        dbConnection.query("INSERT INTO role SET ?",
-                            { title: responses.role, salary: pareseInt(responses.roleSalary), department_id: parseInt(resposnes.roleDepartment) }, (err, res) => {
-                                if (err)
-                                    throw err;
-                                console.log("You successfully added a new role.")
-                                init();
-                            });
-                    });
-            } else if (responses.option = "add an employee") {
-                inquirer.prompt(newEmployee)
-                    .then((responses) => {
-                        dbConnection.query("INSERT INTO employee SET ?",
-                            { first_name: responses.firstName, last_name: responses.lastName, employee_role_id: parseInt(responses.employeeRole), manager_id: parseInt(responses.manager) }, (err, res) => {
-                                if (err)
-                                    throw err;
-                                console.log("You successfully added a new employee.")
-                                init();
-                            });
-                    });
-            } else if (responses.option = "update an employee role") {
-                updateEmployee();
-            } else {
-                init();
-            }
-        })
-};
-
-
-
